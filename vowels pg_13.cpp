3b6c701ccd62cc01379a5b41a90eb165c85b2c9f
Nithin192124182 <112066100+Nithin192124182@users.noreply.github.com>
2022-09-27 20:30:02 +0530

    Add files via upload

---
+++ vowels pg_13.cpp
@@ -0,0 +1,25 @@
+#include <conio.h>
+ 
+ 
+int main()
+{
+    int a[1000],i,n;
+   
+    printf("Enter size of the array : ");
+    scanf("%d", &n);
+ 
+    printf("Enter elements in array : ");
+    for(i=0; i<n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+ 
+    printf("\nAll negative elements in array are : ");
+    for(i=0; i<n; i++)
+    {
+         
+        if(a[i] < 0)
+        {
+            printf("%d\t", a[i]);
+        }
+    }
