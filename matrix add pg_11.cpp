21455d3b8a99acc1082b73d1a0acaa3614408610
Nithin192124182 <112066100+Nithin192124182@users.noreply.github.com>
2022-09-27 20:24:44 +0530

    Add files via upload

--- matrix add pg_11.cpp
+++ matrix add pg_11.cpp
@@ -1,24 +1,22 @@
-#include < stdio.h >   
-int main()  
-{  
-    int m, n, c, d, first[10][10], second[10][10], sum[10][10];  
-    printf("Enter the number of rows and columns of matrix\n");  
-    scanf("%d%d", & m, & n);  
-    printf("Enter the elements of first matrix\n");  
-    for (c = 0; c < m; c++)  
-        for (d = 0; d < n; d++) scanf("%d", & first[c][d]);  
-    printf("Enter the elements of second matrix\n");  
-    for (c = 0; c < m; c++)  
-        for (d = 0; d < n; d++) scanf("%d", & second[c][d]);  
-    printf("Sum of entered matrices:-\n");  
-    for (c = 0; c < m; c++)  
-    {  
-        for (d = 0; d < n; d++)  
-        {  
-            sum[c][d] = first[c][d] + second[c][d];  
-            printf("%d\t", sum[c][d]);  
-        }  
-        printf("\n");  
-    }  
-    return 0;  
-}
+#include<stdio.h>
+int main ()
+{
+    int i,n,a[100],count=0;
+    printf("enter size:");
+    scanf("%d",&n);
+    printf("enter elements\n");
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++){
+       if(a[i]==2){
+          continue;
+       }       
+        else if(a[i]%2==0){
+         count++;
+        }
+    }
+        if(count>2){    
+        }
+       printf("total composite number are: %d",count);
+ }
