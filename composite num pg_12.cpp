82a7732b1dbdff67a76fa31aee58abb520b28f14
Nithin192124182 <112066100+Nithin192124182@users.noreply.github.com>
2022-09-27 20:27:31 +0530

    Add files via upload

--- composite num pg_12.cpp
+++ composite num pg_12.cpp
@@ -1,22 +1,19 @@
-#include<stdio.h>
-int main ()
+#include <stdio.h>
+int main()
 {
-    int i,n,a[100],count=0;
-    printf("enter size:");
-    scanf("%d",&n);
-    printf("enter elements\n");
-    for(i=0;i<n;i++){
-        scanf("%d",&a[i]);
-    }
-    for(i=0;i<n;i++){
-       if(a[i]==2){
-          continue;
-       }       
-        else if(a[i]%2==0){
-         count++;
-        }
-    }
-        if(count>2){    
-        }
-       printf("total composite number are: %d",count);
- }
+  int c = 0, count = 0;
+  char s[1000];
+
+  printf("Input a string\n");
+  gets(s);
+
+  while (s[c] != '\0') {
+    if (s[c] == 'a' || s[c] == 'A' || s[c] == 'e' || s[c] == 'E' || s[c] == 'i' || s[c] == 'I' || s[c] =='o' || s[c]=='O' || s[c] == 'u' || s[c] == 'U')
+      count++;
+    c++;
+  }
+
+  printf("Number of vowels in the string: %d", count);
+
+  return 0;
+}
